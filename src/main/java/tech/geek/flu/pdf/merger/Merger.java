00aabe4be16d0a3f156f1f83d69ba62087e90d1d
Spike Storm <software.j2ee@gmail.com>
2021-01-14 23:15:12 -0800

    new version of iText

---
+++ src/main/java/tech/geek/flu/pdf/merger/Merger.java
@@ -0,0 +1,4 @@
+package tech.geek.flu.pdf.merger;
+
+public class Merger {
+}
--- src/main/java/tech/geek/flu/pdf/merger/PDFMergerBot.java
+++ src/main/java/tech/geek/flu/pdf/merger/PDFMergerBot.java
@@ -1,38 +1,56 @@
 package tech.geek.flu.pdf.merger;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.utils.PdfMerger;
+import com.itextpdf.layout.Document;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class PDFMergerBot {
-  private static final FilenameFilter txtFileFilter;
 
-  static {
-    txtFileFilter = new FilenameFilter() {
-      @Override
-      public boolean accept(File dir, String name) {
-        return name.endsWith(".pdf");
-      }
-    };
-  }
-
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     if (Objects.isNull(args) || args.length <= 0) {
       log.error("Arguments needed:");
       log.error("source=<directory> target=<dir>/final.pdf");
     }
+    String source = args[0];
+    String target = args[1];
+    List<PdfDocument> docs =  loadFolderSource(source.split("=")[1]);
+    log.info("Lista de archivos = {}", docs);
+    PdfDocument targetDoc = new PdfDocument(new PdfWriter(target.split("=")[1]));
+    PdfMerger merger = new PdfMerger(targetDoc);
 
-    List<PdfDocument> docs =  loadFolderSource();
+    docs.forEach(pdfDocumentReader -> merger.merge(pdfDocumentReader, 1, pdfDocumentReader.getNumberOfPages()));
+    docs.forEach(PdfDocument::close);
+    targetDoc.close();
   }
 
-  private static List<PdfDocument> loadFolderSource() {
+  private static List<PdfDocument> loadFolderSource(String source) throws IOException {
+    List<Path> files = Files.list(Paths.get(source))
+        .filter(path -> path.toString().toLowerCase(Locale.ROOT).endsWith(".pdf")).sorted(Comparator.comparing(Path::toString)).collect(Collectors.toList());
 
-    return null;
+    return files.stream().map(path -> {
+      try {
+        return new PdfDocument(new PdfReader(path.toString()));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return null;
+    }).collect(Collectors.toList());
   }
 
 }
