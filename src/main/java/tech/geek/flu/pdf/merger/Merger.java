1fdc2190657f114004378e14d04960368c36ca28
GigaByte <gigabyte@tsunami-binario.com>
2021-12-14 19:55:14 -0800

    agregue atributos ala clase

--- src/main/java/tech/geek/flu/pdf/merger/Merger.java
+++ src/main/java/tech/geek/flu/pdf/merger/Merger.java
@@ -1,4 +1,19 @@
 package tech.geek.flu.pdf.merger;
 
 public class Merger {
+    private String name;
+    private int size;
+    private double token;
+
+    public Merger(String nombre, int tamanio, double llave) {
+        this.name = nombre;
+        this.size = tamanio;
+        this.token = llave;
+    }
+
+    public static void main(String[] args) {
+        Merger merger = new Merger("m1", 23, 23.34); // esto es una instancia
+        Merger merger01 = new Merger("m2", 23, 23.34); // instancia dos
+    }
+
 }
